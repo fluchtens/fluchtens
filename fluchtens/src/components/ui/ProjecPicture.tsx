a4930e361a956267035805b68c5f352200cd36f1
Cleazyy <cleazy2002@gmail.com>
2023-12-10 19:12:26 +0100

    add docker & nginx

--- fluchtens/src/components/ui/ProjecPicture.tsx
+++ fluchtens/src/components/ui/ProjecPicture.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import Image from "next/image";
+// import Image from "next/image";
 
 export const ProjetPicture = ({ name }: { name: string }) => (
-  <Image
+  <img
     src={`/projects/${name}/${name}_1.png`}
     alt={`${name}_1.png`}
     className="md:max-w-[384px] md:max-h-[216px]"
