d5d4b72dd1b3a14c3d6e47e8510f38a2d74d3a66
Cleazyy <cleazy2002@gmail.com>
2024-02-23 19:30:25 +0100

    fix footer

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,9 +1,9 @@
 export const Footer = () => (
-  <header className="px-4 border-t border-slate-200 border-opacity-10">
+  <footer className="px-4 border-t border-slate-200 border-opacity-10">
     <div className="max-w-screen-lg m-auto flex justify-center items-center text-center">
       <p className="py-3 text-sm font-normal text-quaternary">
         © 2023. All rights reserved.
       </p>
     </div>
-  </header>
+  </footer>
 );
