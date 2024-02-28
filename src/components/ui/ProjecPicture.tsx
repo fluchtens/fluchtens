9e0bb7032a0fb6252ab7afa321f09671f5cfcdc0
fluchtens <cleazy2002@gmail.com>
2024-02-28 12:22:26 +0100

    update project picture

--- src/components/ui/ProjecPicture.tsx
+++ src/components/ui/ProjecPicture.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { Language, LanguageElement } from "./LanguageElement";
+import { ProjectModal } from "./ProjectModal";
 
 interface ProjecPictureProps {
   name: string;
@@ -16,13 +17,9 @@ export const ProjetPicture = ({
 }: ProjecPictureProps) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openProjectModal = () => {
-    setIsModalOpen(true);
-  };
-
   return (
     <>
-      <button onClick={openProjectModal}>
+      <button onClick={() => setIsModalOpen(true)}>
         <img
           src={`/projects/${name}/${name}_1.png`}
           alt={`${name}_1.png`}
@@ -30,25 +27,12 @@ export const ProjetPicture = ({
         />
       </button>
       {isModalOpen && (
-        <div
-          className="fixed top-0 left-0 w-full h-full p-4 bg-black bg-opacity-50 flex justify-center items-center"
-          onClick={() => setIsModalOpen(false)}
-        >
-          <div className="max-w-screen-md	p-6 flex-col flex gap-2 bg-secondary rounded-xl">
-            <h1 className="text-2xl font-bold">{name}</h1>
-            <img
-              src={`/projects/${name}/${name}_1.png`}
-              alt={`${name}_1.png`}
-              className="w-auto rounded-lg pointer-events-none"
-            />
-            <p className="text-base font-normal text-zinc-400">{desc}</p>
-            <ul className="flex-wrap flex justify-center sm:justify-normal items-center gap-2">
-              {languages.map((language, index) => (
-                <LanguageElement key={index} language={language} />
-              ))}
-            </ul>
-          </div>
-        </div>
+        <ProjectModal
+          name={name}
+          desc={desc}
+          languages={languages}
+          close={() => setIsModalOpen(false)}
+        />
       )}
     </>
   );
