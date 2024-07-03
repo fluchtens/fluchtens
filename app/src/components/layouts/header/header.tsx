c60712e591c49b45b7398a1e2f9c8127ae0a015b
Cleazyy <cleazy2002@gmail.com>
2024-07-03 19:36:38 +0200

    add social button to header

--- app/src/components/layouts/header/header.tsx
+++ app/src/components/layouts/header/header.tsx
@@ -1,7 +1,21 @@
 "use server";
 
+import { Button } from "@/components/ui/button";
+import { Config } from "@/types/config.interface";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
-import { GithubButton } from "./github-button";
+import { ReactNode } from "react";
+const config: Config = require("@/config.json");
+
+const SocialButton = ({ href, icon }: { href: string; icon: ReactNode }) => {
+  return (
+    <Button variant="ghost" size="icon" asChild>
+      <Link href={href} target="_blank">
+        {icon}
+      </Link>
+    </Button>
+  );
+};
 
 export const Header = () => (
   <header className="px-4 sm:px-6 py-3 border-b">
@@ -10,7 +24,8 @@ export const Header = () => (
         <h1 className="py-2 text-lg font-medium">fluchtens.com</h1>
       </Link>
       <div className="flex items-center">
-        <GithubButton />
+        <SocialButton href={config.links.github} icon={<GitHubLogoIcon className="h-[1.2rem] w-[1.2rem]" />} />
+        <SocialButton href={config.links.linkedin} icon={<LinkedInLogoIcon className="h-[1.2rem] w-[1.2rem]" />} />
       </div>
     </div>
   </header>
