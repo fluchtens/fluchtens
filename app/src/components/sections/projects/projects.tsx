b81f0fa7b72c2b585c18ee498f12ceb4a35521eb
Cleazyy <cleazy2002@gmail.com>
2024-10-02 18:34:54 +0200

    fix projects carousel

--- app/src/components/sections/projects/projects.tsx
+++ app/src/components/sections/projects/projects.tsx
@@ -13,7 +13,7 @@ const config: Config = require("@/config.json");
 export const Projects = () => (
   <section id="projects" className="flex-col flex items-center gap-4 md:gap-5 w-full m-auto">
     <CategoryTitle title="Projects" />
-    <Carousel opts={{ duration: 0, watchDrag: false }} className="px-12 w-full">
+    <Carousel opts={{ duration: 0, watchDrag: false }} className="px-12 xl:px-0 w-full">
       <CarouselContent>
         {config.projects.map((project, index) => (
           <CarouselItem key={index} className="sm:basis-1/1 md:basis-1/2 lg:basis-1/3">
@@ -30,8 +30,8 @@ export const Projects = () => (
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious className="left-0" />
-      <CarouselNext className="right-0" />
+      <CarouselPrevious className="left-0 xl:-left-12" />
+      <CarouselNext className="right-0 xl:-right-12" />
     </Carousel>
     <Button asChild variant="default" className="px-3 py-2">
       <Link href={config.links.repositories} target="_blank" className="flex items-center gap-1.5">
