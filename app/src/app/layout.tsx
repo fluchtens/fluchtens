e05503daac58172af63fc74089ee847c3117083c
Cleazyy <cleazy2002@gmail.com>
2024-10-02 17:55:36 +0200

    fix viewport

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { Footer } from "@/components/layouts/footer/footer";
 import { Header } from "@/components/layouts/header/header";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Montserrat } from "next/font/google";
 import "../styles/globals.css";
 
@@ -8,7 +8,15 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Francois Luchtens - Full Stack Developer",
-  description: "Junior web developer, passionate about new technologies and computer programming. Alumni at 19 (42 Network) in Brussels.",
+  description:
+    "Web developer, passionate about computer programming and projet creation, always keen to learn new technologies. Alumni at 19 (42 Network) in Brussels.",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
